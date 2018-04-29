15cce54e5cd866518f6c983b6f5998b104e02a83
ZepengZhao <email@example.com>
2018-04-29 14:30:25 -0700

    add main cpp 1st

--- siete-y-medio.cpp
+++ siete-y-medio.cpp
@@ -28,5 +28,43 @@ int main() {
 	cout << "Enter bet:";
 	cin >> bet_money;
 	cout << endl;
+
+	Hand player_cards;
+	while (true)
+	{
+
+		Card new_card;
+		//Hand player_cards;
+		cout << "New card:" << endl;
+		cout << new_card.get_spanish_rank() << " de " << new_card.get_spanish_suit();
+		cout << "(" << new_card.get_english_rank() << " of " << new_card.get_english_suit() << ").";
+		player_cards.save_card(new_card);
+		player_cards.hand_total(new_card);
+		cout << endl;
+		cout << "Your cards:" << endl;
+		for (int i = 0; i < player_cards.get_cards().size(); i++)
+		{
+			cout << player_cards.get_cards()[i].get_spanish_rank()
+				<< " de " << player_cards.get_cards()[i].get_spanish_suit();
+			cout << "(" << player_cards.get_cards()[i].get_english_rank()
+				<< " of " << player_cards.get_cards()[i].get_english_suit() << ").";
+			cout << endl;
+		}
+		cout << endl;
+		cout << "Your total is " << player_cards.get_total() << ".";
+		if (player_cards.get_total() > 7.5)
+			break;
+		cout << "Do you want another card(y/n)?";
+		cin >> ans;
+		if (ans == 'n')
+			break;
+		//cout << "New card:";
+		//cout << new_card.get_spanish_rank() << " de " << new_card.get_spanish_suit();
+		//cout << "(" << new_card.get_spanish_rank() << " of " << new_card.get_spanish_suit() << ").";
+	}
+	Hand dealer_cards;
+
+
 	return 0;
+
 }
