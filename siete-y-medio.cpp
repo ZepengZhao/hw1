d6aac582c790157788060d12837fb77da6550e6b
ZepengZhao <email@example.com>
2018-04-29 14:32:35 -0700

    add main cpp 2nd

--- siete-y-medio.cpp
+++ siete-y-medio.cpp
@@ -63,7 +63,32 @@ int main() {
 		//cout << "(" << new_card.get_spanish_rank() << " of " << new_card.get_spanish_suit() << ").";
 	}
 	Hand dealer_cards;
-
+	while (player_cards.get_total() <= 7.5&&dealer_cards.get_total() < 5.5)
+	{
+		Card new_card;
+		//Hand player_cards;
+		cout << "New card:" << endl;
+		cout << new_card.get_spanish_rank() << " de " << new_card.get_spanish_suit();
+		cout << "(" << new_card.get_english_rank() << " of " << new_card.get_english_suit() << ").";
+		dealer_cards.save_card(new_card);
+		dealer_cards.hand_total(new_card);
+		cout << endl;
+		cout << "Dealer's cards:" << endl;
+		for (int i = 0; i < dealer_cards.get_cards().size(); i++)
+		{
+			cout << dealer_cards.get_cards()[i].get_spanish_rank()
+				<< " de " << dealer_cards.get_cards()[i].get_spanish_suit();
+			cout << "(" << dealer_cards.get_cards()[i].get_english_rank()
+				<< " of " << dealer_cards.get_cards()[i].get_english_suit() << ").";
+			cout << endl;
+		}
+		cout << endl;
+		cout << "The dealer's total is " << dealer_cards.get_total() << ".";
+		cout << endl;
+	}
+	player.current_amount(dealer_cards, bet_money);
+	//dealer.current_amount(player, bet_money);
+	cout << endl;
 
 	return 0;
 
